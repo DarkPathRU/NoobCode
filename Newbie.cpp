13a69ed33a3bdd8cd86c80189b02f2030eb3e8aa
DarkPathRU <166197578+DarkPathRU@users.noreply.github.com>
2024-05-27 15:15:16 +0300

    test.cpp
    my first test

---
+++ Newbie.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+#include <string>
+#include <math.h>
+#include <memory>
+#include <tuple>
+#include <forward_list>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#include <regex>
+#include <fstream>
+#include<set>
+#include<unordered_set>
+#include<sstream>
+#include<map>
+using namespace std;
+
+
+
+
+
+
+
+
+int main() {
+	cout << "hello" << endl;
+	return 0;
+}
